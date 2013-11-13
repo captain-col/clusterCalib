4207de3a377cd041a85a4b2c73142558ff625a02
Clark McGrew <clark.mcgrew@stonybrook.edu>
2013-11-13 14:54:07 -0500

    Split long pulses into several hits.
    When a track is traveling along a wire, it's pulse can be very long.
    Instead of just returning the mean time of the pulse and RMS, long pulses
    are split into several hits.

--- src/TWireMakeHits.cxx
+++ src/TWireMakeHits.cxx
@@ -14,6 +14,7 @@
 
 #include <cmath>
 #include <vector>
+#include <algorithm>
 
 CP::TWireMakeHits::TWireMakeHits() {
     fSource = NULL;
@@ -28,21 +29,74 @@ CP::TWireMakeHits::TWireMakeHits() {
         = CP::TRuntimeParameters::Get().GetParameterD(
             "clusterCalib.peakSearch.deconvolution");
 
+    fPeakRMSLimit 
+        = CP::TRuntimeParameters::Get().GetParameterD(
+            "clusterCalib.peakSearch.rmsLimit");
+
 }
 CP::TWireMakeHits::~TWireMakeHits() {
     if (fSource) delete fSource;
     if (fDest) delete fDest;
 }
 
-void CP::TWireMakeHits::operator() (CP::THitSelection& hits, 
-                                    const CP::TCalibPulseDigit& digit) {
+CP::THandle<CP::THit> 
+CP::TWireMakeHits::MakeHit(const CP::TCalibPulseDigit& digit, double step,
+                           int beginIndex, int endIndex) {
+    double charge = 0.0;
+    double sample = 0.0;
+    double sampleSquared = 0.0;
+    double samples = 1.0;
+    
+    // Find the peak charge, average sample index and sample index squared for
+    // the peak.  The average is charge weighted.
+    for (int j=beginIndex; j<endIndex; ++j) {
+        double v = digit.GetSample(j);
+        charge += v;
+        sample += v*j;
+        sampleSquared += v*j*j;
+        ++samples;
+    }
+    sample /= charge;
+    sampleSquared /= charge;
+
+    // Convert the sample index into a time.
+    double time = (sample + 0.5)*step + digit.GetFirstSample();
+    
+    // Find the sample RMS, and then convert into a time RMS.
+    double rms = step*std::sqrt(sampleSquared - sample*sample + 1.0);
+    
+    // Base the uncertainty in the time on the number of samples used to find
+    // the RMS.
+    double timeUnc = rms/std::sqrt(samples);
+
+    // The charge uncertainty is bogus here.  For now it's based on the sqrt
+    // of the charge (assumes Poissonian statistics), but that is almost
+    // certainly an underestimate.
+    double chargeUnc = std::sqrt(charge);
 
     CP::TGeometryId geomId 
         = CP::TChannelInfo::Get().GetGeometry(digit.GetChannelId());
 
+    // Build the hit.
+    CP::TWritableDataHit hit;
+    hit.SetGeomId(geomId);
+    hit.SetDigit(digit.GetParent());
+    hit.SetCharge(charge);
+    hit.SetChargeUncertainty(chargeUnc);
+    hit.SetTime(time);
+    hit.SetTimeRMS(rms);
+    hit.SetTimeUncertainty(timeUnc);
+
+    return CP::THandle<CP::TDataHit>(new CP::TDataHit(hit));
+}
+
+void CP::TWireMakeHits::operator() (CP::THitSelection& hits, 
+                                    const CP::TCalibPulseDigit& digit) {
+
     // Make sure we have enough memory allocated for the spectrum.
     if (fNSource < (int) digit.GetSampleCount()) {
         if (fSource) delete fSource;
+        if (fDest) delete fDest;
         fNSource = 2*digit.GetSampleCount();
         fSource = new float[fNSource];
         fDest = new float[fNSource];
@@ -69,10 +123,6 @@ void CP::TWireMakeHits::operator() (CP::THitSelection& hits,
 #endif
         
     TSpectrum* spectrum = new TSpectrum(1000);
-
-    // Find the time per sample in the digit.
-    double digitStep = digit.GetLastSample()-digit.GetFirstSample();
-    digitStep /= digit.GetSampleCount();
 
     // Set the parameters to use for the peak search.  These should be in the
     // parameters file, but the search isn't extremely sensitive to them.  The
@@ -103,96 +153,111 @@ void CP::TWireMakeHits::operator() (CP::THitSelection& hits,
         }
 #endif
         
-    // Get the peak positions and sort by time.
+    // Get the peak positions, and determine which ones are above threshold.
     float* xx = spectrum->GetPositionX();
     std::vector<float> peaks;
-    for (int i=0; i<found; ++i)  peaks.push_back(xx[i]);
 
-    // This ugly bit of code is calculating the amount of charge in each peak
-    // and the charge uncertainty; calculating the peak time RMS and time
-    // uncertainty; and using that to create a hit.  The heuristic is that a
-    // peak contains all charge for bins that are more than "chargeThresh"
-    // times the peak value.  If there is more than one peak found, then the
-    // chanrge is split at the halfway point between the peaks. 
-    //
-    // THIS IS A QUICK AND DIRTY HACK TO GET SOMETHING "OUT THE DOOR". 
-    //
-    // UGLY CODE ALERT.  This does not make me proud.
-    //
+    int digitEndSkip = 3;
+    for (int i=0; i<found; ++i)  {
+        // No peaks at the ends of the digit.
+        if (xx[i] < digitEndSkip) continue;
+        if (xx[i] > digit.GetSampleCount()-digitEndSkip - 1) continue;
+        // Check the peak size and deconvolution power.
+        int index = xx[i] + 0.5;
+        if (digit.GetSample(index) < fPeakMaximumCut) continue;
+        if (fDest[index] < fPeakDeconvolutionCut) continue;
+        peaks.push_back(xx[i]);
+    }
+
+    // Sort the peaks by time.
+    std::sort(peaks.begin(), peaks.end());
+
+    // This ugly bit of code is taking a found peak and making sure that if
+    // it's too wide (i.e. the RMS is too big), it's split into smaller hits.
+    // The heuristic is that a peak contains all charge for bins that are more
+    // than "chargeThresh" times the peak value.  If more than one peak is
+    // found, then the digit is split at the halfway point between the peaks.
+    // If the peak is too wide, then the peak is split.
     double chargeThresh = 0.1;
     for (std::vector<float>::iterator p = peaks.begin();
          p != peaks.end(); ++p) {
-        // No peaks at the end of a digit.
-        if (*p < 3) continue;
-        if (*p > digit.GetSampleCount()-3) continue;
-        double center = *p;
         int i = *p + 0.5;
-        int begin = 0;
-        int end = digit.GetSampleCount();
+        int beginIndex = 0;
+        int endIndex = digit.GetSampleCount();
         double peak = digit.GetSample(i);
-        double deconv = fDest[i];
 
-        // Only look at peaks above the thresholds.
-        if (peak < fPeakMaximumCut) continue;
-        if (deconv < fPeakDeconvolutionCut) continue;
-
+        // For the current peak, find the upper and lower bounds of the
+        // integration region.  If the peak is close to another, the bound is
+        // halfway between the two peaks.  After this is finished, the
+        // "beginIndex" and "endIndex" will be the indices to be looking at.
         if (peaks.size() > 1) {
             for (std::vector<float>::iterator o = peaks.begin(); 
                  o != peaks.end(); ++o) {
                 if (o == p) continue;
                 if (*o < *p) {
                     int t = (*p+*o)/2;
-                    if (begin<t) begin = t;
+                    if (beginIndex<t) beginIndex = t;
                 }
                 else {
                     int t = (*p+*o)/2;
-                    if (end>t) end = t;
+                    if (endIndex>t) endIndex = t;
                 }
             }
         }
-        double charge = digit.GetSample(i);
-        double time = i*digit.GetSample(i);
-        double timeSquared = i*i*digit.GetSample(i);
-        double samples = 1.0;
-        // Sum on the low side of the peak.
-        for (int j = i-1; begin <= j; --j) {
-            double v = digit.GetSample(j);
-            if (v < chargeThresh*peak) break;
-            charge += v;
-            time += v*j;
-            timeSquared += v*j*j;
-            ++samples;
-        }
-        for (int j = i+1; j < end; ++j) {
-            double v = digit.GetSample(j);
-            if (v < chargeThresh*peak) break;
-            charge += v;
-            time += v*j;
-            timeSquared += v*j*j;
-            ++samples;
-        }
-        double chargeUnc = std::sqrt(charge);
-        center *= digitStep;
 
+        // Search down from the peak center to find the actual integration
+        // bounds based on the actual extent of the peak.
+        int j = beginIndex;
+        for (beginIndex = i-1; j <= beginIndex; --beginIndex) {
+            double v = digit.GetSample(beginIndex);
+            if (v < chargeThresh*peak) break;
+        }
+
+        // Search up from the peak center to find the actual integration
+        // bounds based on the actual extent of the peak.
+        j = endIndex;
+        for (endIndex = i+1; endIndex < j; ++endIndex) {
+            double v = digit.GetSample(endIndex);
+            if (v < chargeThresh*peak) break;
+        }
+
+        double charge = 0.0;
+        double time = 0.0;
+        double timeSquared = 0.0;
+        double samples = 1.0;
+
+        // Find the raw RMS for the peak.
+        for (int j=beginIndex; j<endIndex; ++j) {
+            double v = digit.GetSample(j);
+            if (v<0.1) v = 0.1;
+            charge += v;
+            time += v*j;
+            timeSquared += v*j*j;
+            ++samples;
+        }
         time /= charge;
         timeSquared /= charge;
 
-        double rms = (timeSquared - time*time);
-        time = time*digitStep;
-        rms = digitStep*std::sqrt(rms + 1);
-        double timeUnc = rms/std::sqrt(samples) + std::abs(center - time);
-        rms = rms + std::abs(center-time);
+        // Find the time per sample in the digit.
+        double digitStep = digit.GetLastSample()-digit.GetFirstSample();
+        digitStep /= digit.GetSampleCount();
 
-        center += digit.GetFirstSample() + 0.5*digitStep;
+        double rawRMS = (timeSquared - time*time);
+        rawRMS = digitStep*std::sqrt(rawRMS+1.0);
 
-        CP::TWritableDataHit hit;
-        hit.SetGeomId(geomId);
-        hit.SetDigit(digit.GetParent());
-        hit.SetCharge(charge);
-        hit.SetChargeUncertainty(chargeUnc);
-        hit.SetTime(center);
-        hit.SetTimeRMS(rms);
-        hit.SetTimeUncertainty(timeUnc);
-        hits.push_back(CP::THandle<CP::TDataHit>(new CP::TDataHit(hit)));
+        int split = rawRMS/fPeakRMSLimit + 1.0;
+        double step = 1.0*(endIndex-beginIndex)/split;
+
+        /// The loop looks a little odd since it's being done with doubles,
+        /// but this means that the "digitized" split size gets distributed
+        /// over the entire range instead of all at one end.
+        for (double baseIndex = beginIndex; 
+             baseIndex<endIndex; 
+             baseIndex += step) {
+            int i = baseIndex;
+            int j = baseIndex + step;
+            CP::THandle<CP::THit> newHit = MakeHit(digit, digitStep, i, j);
+            hits.push_back(newHit);
+        }            
     }
 }
--- src/TWireMakeHits.hxx
+++ src/TWireMakeHits.hxx
@@ -24,21 +24,32 @@ public:
 
 private:
 
-    // A buffer for the spectrum.
+    /// Build a hit out of the digit samples between beginIndex and endIndex.
+    /// The digit step size is provided as an input.
+    CP::THandle<CP::THit> 
+    MakeHit(const CP::TCalibPulseDigit& digit, double digitStep,
+            int beginIndex, int endIndex) ;
+
+    /// A buffer for the spectrum.
     float* fSource;
 
-    // The size of the buffer for the spectrum
+    /// The size of the buffer for the spectrum
     int fNSource;
 
-    // A buffer for the found peaks;
+    /// A buffer for the found peaks;
     float* fDest;
 
-    // The charge required at the peak for it to be considered valid.
+    /// The charge required at the peak for it to be considered valid.
     double fPeakMaximumCut;
 
-    // The power in the TSpectrum deconvolution for the peak for it to be
-    // considered valid.
+    /// The power in the TSpectrum deconvolution for the peak for it to be
+    /// considered valid.
     double fPeakDeconvolutionCut;
+
+    /// Set the limit on how wide a peak can be in drift time before it's
+    /// split into multiple hits.  Peaks wider than this are split up into
+    /// multiple hits by drift time.
+    double fPeakRMSLimit;
 };
 
 #endif
