3d8c8965996a0e85516ce7a12ac4bb6c4d6bb467
Clark McGrew <clark.mcgrew@stonybrook.edu>
2014-02-20 10:22:03 -0500

    Change exception base class to match the algorithm name.

--- src/EClusterCalib.hxx
+++ src/EClusterCalib.hxx
@@ -1,10 +1,10 @@
-#ifndef EClusterRecon_hxx_seen
-#define EClusterRecon_hxx_seen
+#ifndef EClusterCalib_hxx_seen
+#define EClusterCalib_hxx_seen
 
 #include <ECore.hxx>
 
 namespace CP {
     /// Generate a base exception for the Analysis library
-    EXCEPTION(EClusterRecon,ECore);
+    EXCEPTION(EClusterCalib,ECore);
 };
 #endif
