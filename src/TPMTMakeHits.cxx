f404f4e6dd9470d5890e10c9a081b85f6f3a688a
Clark McGrew <clark.mcgrew@stonybrook.edu>
2014-07-11 14:54:27 -0400

    Fix the pmt calibration so a 1 pe peak has a charge of 1.0.

--- src/TPMTMakeHits.cxx
+++ src/TPMTMakeHits.cxx
@@ -26,17 +26,17 @@ void CP::TPMTMakeHits::operator() (CP::THitSelection& hits,
     double digitStep = digit.GetLastSample()-digit.GetFirstSample();
     digitStep /= digit.GetSampleCount();
 
-    // This should do a peak search, but the PMT peaks are really (really,
-    // really) sharp, and extremely narrow.  This is probably good enough for
-    // now.
+    // This should do a peak search, but the PMT peaks are really sharp, and
+    // narrow.  This is probably good enough for now.
+    double maxVal = 0.0;
     for (std::size_t i=0; i<digit.GetSampleCount(); ++i) {
         if (digit.GetSample(i) < fThreshold)  continue;
         double hitTime = digitStep*i + digit.GetFirstSample();
         std::size_t hitStart = i;
-        if (i>2) hitStart -= 2;
+        if (i>10) hitStart -= 10;
         else hitStart = 0;
         std::size_t belowCount = 0;
-        while (i < digit.GetSampleCount() && belowCount < 4) {
+        while (i < digit.GetSampleCount() && belowCount < 10) {
             if (digit.GetSample(i) > fThreshold) {
                 belowCount = 0;
                 ++i;
@@ -48,6 +48,7 @@ void CP::TPMTMakeHits::operator() (CP::THitSelection& hits,
         std::size_t hitStop = i;
         double hitCharge = 0.0;
         for (std::size_t j = hitStart; j<hitStop; ++j) {
+            maxVal = std::max(maxVal,digit.GetSample(j));
             hitCharge += digit.GetSample(j);
         }
         double hitLength = (hitStop-hitStart)*digitStep;
