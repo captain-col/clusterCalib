12f0b06e87180fee76d1edfc167f23036f31503f
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-09-09 15:30:34 -0400

    Add simplified method to find peaks.
    This is more straight forward than the TSpectrum method, and about as effective.  It hasn't been evaluated relative to TWireSpectrum, but is so much simpler that it is the best choice for now.

---
+++ src/TWirePeaks.cxx
@@ -0,0 +1,445 @@
+#include "TWirePeaks.hxx"
+#include "TMakeWireHit.hxx"
+#include "TChannelCalib.hxx"
+
+#include <THitSelection.hxx>
+#include <TCalibPulseDigit.hxx>
+#include <TChannelInfo.hxx>
+#include <TFADCHit.hxx>
+#include <HEPUnits.hxx>
+#include <THandle.hxx>
+#include <TCaptLog.hxx>
+#include <TRuntimeParameters.hxx>
+#include <CaptGeomId.hxx>
+
+#include <TSpectrum.h>
+#include <TH1F.h>
+#include <TH2F.h>
+
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <memory>
+#include <sstream>
+#include <iostream>
+
+CP::TWirePeaks::TWirePeaks(bool correctLifetime,
+                           bool correctEfficiency) {
+    fCorrectElectronLifetime = correctLifetime;
+    fCorrectCollectionEfficiency = correctEfficiency;
+    fMaxPeaks = 50;
+    
+    fPeakMaximumCol
+        = CP::TRuntimeParameters::Get().GetParameterD(
+            "clusterCalib.peakSearch.charge.collection");
+
+    fPeakMaximumInd
+        = CP::TRuntimeParameters::Get().GetParameterD(
+            "clusterCalib.peakSearch.charge.induction");
+
+    fPeakAreaCol
+        = CP::TRuntimeParameters::Get().GetParameterD(
+            "clusterCalib.peakSearch.area.collection");
+
+    fPeakAreaInd
+        = CP::TRuntimeParameters::Get().GetParameterD(
+            "clusterCalib.peakSearch.area.induction");
+
+    fPeakWidthCol
+        = CP::TRuntimeParameters::Get().GetParameterD(
+            "clusterCalib.peakSearch.width.collection");
+
+    fPeakWidthInd
+        = CP::TRuntimeParameters::Get().GetParameterD(
+            "clusterCalib.peakSearch.width.induction");
+
+    fNoiseThresholdCut
+        = CP::TRuntimeParameters::Get().GetParameterD(
+            "clusterCalib.peakSearch.noise");
+
+    fPeakRMSLimit
+        = CP::TRuntimeParameters::Get().GetParameterD(
+            "clusterCalib.peakSearch.rmsLimit");
+
+    fDigitEndSkip
+        = CP::TRuntimeParameters::Get().GetParameterI(
+            "clusterCalib.peakSearch.endSkip");
+    if (fDigitEndSkip < 1) fDigitEndSkip = 1;
+    
+    fIntegrationChargeThreshold = 0.001; // effectively zero.
+
+    fIntegrationShoulderThreshold = 0.5;
+
+    fIntegrationValleyThreshold = 0.1;
+
+}
+CP::TWirePeaks::~TWirePeaks() {}
+
+std::pair<int, int>
+CP::TWirePeaks::PeakExtent(int peakIndex,
+                           const CP::TCalibPulseDigit& deconv,
+                           double baselineSigma, double sampleSigma) {
+    double peak = deconv.GetSample(peakIndex);
+    // The lowest point found so far.
+    double valley = peak;
+    // A boolean that is true when the shoulder threshold has been passed.
+    bool passedShoulder = false;
+    // A threshold to say that the main body of the peak has been passed.
+    double shoulderThreshold = peak*fIntegrationShoulderThreshold;
+    // A threshold to say that we are climbing the next peak.
+    double valleyThreshold = peak*fIntegrationValleyThreshold;
+    
+    std::size_t beginIndex = peakIndex;
+    for (std::size_t i=peakIndex; i>0; --i) {
+        double v = deconv.GetSample(i);
+        if (v < valley) {
+            valley = v;
+            beginIndex = i;
+        }
+        if (v < shoulderThreshold) passedShoulder = true;
+        if (v < fIntegrationChargeThreshold*peak) break;
+        if (passedShoulder && v > valley+valleyThreshold) break;
+    }
+
+    valley = peak;
+    passedShoulder = false;
+
+    std::size_t endIndex = peakIndex;
+    for (std::size_t i=peakIndex; i<deconv.GetSampleCount(); ++i) {
+        double v = deconv.GetSample(i);
+        if (v < valley) {
+            valley = v;
+            endIndex = i;
+        }
+        if (v < shoulderThreshold) passedShoulder = true;
+        if (v < fIntegrationChargeThreshold*peak) break;
+        if (passedShoulder && v > valleyThreshold) break;
+    }
+    return std::make_pair(beginIndex,endIndex);
+}
+
+double CP::TWirePeaks::PeakFWHM(int peakIndex,
+                                const std::pair<int,int>& extent,
+                                const CP::TCalibPulseDigit& deconv) {
+    if (peakIndex < extent.first) return -1;
+    if (peakIndex > extent.second) return -1;
+    double peakHeight = deconv.GetSample(peakIndex);
+    
+    // Find the interpolated full width half max.
+    int lowBound = peakIndex;
+    for (int j=peakIndex-1; extent.first<=j; --j) {
+        if (deconv.GetSample(j) < 0.5*peakHeight) break;
+        lowBound = j;
+    }
+    int highBound = peakIndex;
+    for (int j = peakIndex+1; j<=extent.second; ++j) {
+        if (deconv.GetSample(j) < 0.5*peakHeight) break;
+        highBound = j;
+    }
+    double lowValue = lowBound -
+        (0.5*peakHeight-deconv.GetSample(lowBound-1))
+        /(deconv.GetSample(lowBound)-deconv.GetSample(lowBound-1));
+    double highValue = highBound +
+        (deconv.GetSample(highBound)-0.5*peakHeight)
+        /(deconv.GetSample(highBound)-deconv.GetSample(highBound+1));
+    double fwhm = highValue - lowValue;
+    return fwhm;
+}
+
+double CP::TWirePeaks::operator() (CP::THitSelection& hits,
+                                   const CP::TCalibPulseDigit& calib,
+                                   const CP::TCalibPulseDigit& deconv,
+                                   double t0,
+                                   double baselineSigma,
+                                   double sampleSigma) {
+    double wireCharge = 0.0;
+
+    // Find the time per sample in the digit.
+    double digitStep = deconv.GetLastSample()-deconv.GetFirstSample();
+    digitStep /= deconv.GetSampleCount();
+
+    if (fWork.capacity() < deconv.GetSampleCount()) {
+        fWork.resize(deconv.GetSampleCount());
+    }
+    
+    CP::TChannelCalib channelCalib;
+    bool wireIsBipolar = channelCalib.IsBipolarSignal(deconv.GetChannelId());
+
+    double peakMaximumCut = fPeakMaximumCol;
+    double peakAreaCut = fPeakAreaCol;
+    double peakWidthCut = fPeakWidthCol;
+    if (wireIsBipolar) {
+        peakMaximumCut = fPeakMaximumInd;
+        peakAreaCut = fPeakAreaInd;
+        peakWidthCut = fPeakWidthInd;
+    }
+
+#ifdef TPC_WIRE    
+    std::ostringstream histName;
+    histName << "wire-"
+             << std::setw(3)
+             << std::setfill('0')
+             << CP::TChannelInfo::Get().GetWireNumber(deconv.GetChannelId());
+
+    std::ostringstream histTitle;
+    histTitle << "Wire "
+             << CP::TChannelInfo::Get().GetWireNumber(deconv.GetChannelId())
+             << " (" << deconv.GetChannelId().AsString() << ")";
+#else
+    std::ostringstream histName;
+    CP::TGeometryId id
+        = CP::TChannelInfo::Get().GetGeometry(deconv.GetChannelId());
+    if (CP::GeomId::Captain::IsUWire(id)) histName << "wire-u";
+    if (CP::GeomId::Captain::IsVWire(id)) histName << "wire-v";
+    if (CP::GeomId::Captain::IsXWire(id)) histName << "wire-x";
+    histName << "-" << std::setw(3) << std::setfill('0')
+             << CP::GeomId::Captain::GetWireNumber(id);
+
+    std::ostringstream histTitle;
+    if (CP::GeomId::Captain::IsUWire(id)) histTitle << "Wire U";
+    if (CP::GeomId::Captain::IsVWire(id)) histTitle << "Wire V";
+    if (CP::GeomId::Captain::IsXWire(id)) histTitle << "Wire X";
+    histTitle << "-" << CP::GeomId::Captain::GetWireNumber(id)
+              << " (" << deconv.GetChannelId().AsString() << ")";
+#endif
+
+#define STANDARD_HISTOGRAM
+#ifdef STANDARD_HISTOGRAM
+#undef STANDARD_HISTOGRAM
+    static TH2F* gPeakAreaHeightU = NULL;
+    if (!gPeakAreaHeightU) {
+        gPeakAreaHeightU
+            = new TH2F("peakAreaHeightU",
+                       "Area versus height for U peaks",
+                       50, 0.0, 5000,
+                       40, 0.0, 20000);
+    }
+    static TH2F* gPeakAreaHeightV = NULL;
+    if (!gPeakAreaHeightV) {
+        gPeakAreaHeightV
+            = new TH2F("peakAreaHeightV",
+                       "Area versus height for V peaks",
+                       50, 0.0, 5000,
+                       40, 0.0, 20000);
+    }
+    static TH2F* gPeakAreaHeightX = NULL;
+    if (!gPeakAreaHeightX) {
+        gPeakAreaHeightX
+            = new TH2F("peakAreaHeightX",
+                       "Area versus height for X peaks",
+                       50, 0.0, 5000,
+                       40, 0.0, 20000);
+    }
+    static TH2F* gPeakFWHMHeightU = NULL;
+    if (!gPeakFWHMHeightU) {
+        gPeakFWHMHeightU
+            = new TH2F("peakFWHMHeightU",
+                       "FWHM versus height for U peaks",
+                       50, 0.0, 5000.0,
+                       40, 0.0, 20.0);
+    }
+    static TH2F* gPeakFWHMHeightV = NULL;
+    if (!gPeakFWHMHeightV) {
+        gPeakFWHMHeightV
+            = new TH2F("peakFWHMHeightV",
+                       "FWHM versus height for V peaks",
+                       50, 0.0, 5000.0,
+                       40, 0.0, 20.0);
+    }
+    static TH2F* gPeakFWHMHeightX = NULL;
+    if (!gPeakFWHMHeightX) {
+        gPeakFWHMHeightX
+            = new TH2F("peakFWHMHeightX",
+                       "FWHM versus height for X peaks",
+                       50, 0.0, 5000.0,
+                       40, 0.0, 20.0);
+    }
+    static TH2F* gPeakFWHMAreaU = NULL;
+    if (!gPeakFWHMAreaU) {
+        gPeakFWHMAreaU
+            = new TH2F("peakFWHMAreaU",
+                       "FWHM versus area for U peaks",
+                       40, 0.0, 20000.0,
+                       40, 0.0, 20.0);
+    }
+    static TH2F* gPeakFWHMAreaV = NULL;
+    if (!gPeakFWHMAreaV) {
+        gPeakFWHMAreaV
+            = new TH2F("peakFWHMAreaV",
+                       "FWHM versus area for V peaks",
+                       40, 0.0, 20000.0,
+                       40, 0.0, 20.0);
+    }
+    static TH2F* gPeakFWHMAreaX = NULL;
+    if (!gPeakFWHMAreaX) {
+        gPeakFWHMAreaX
+            = new TH2F("peakFWHMAreaX",
+                       "FWHM versus area for X peaks",
+                       40, 0.0, 20000.0,
+                       40, 0.0, 20.0);
+    }
+#endif
+    
+    // Find the magnitude of noise for this channel.
+    for (std::size_t i = 0; i<deconv.GetSampleCount(); ++i) {
+        fWork[i] = std::abs(deconv.GetSample(i));
+    }
+    std::sort(&fWork[fDigitEndSkip],
+              &fWork[deconv.GetSampleCount()-fDigitEndSkip]);
+    int inoise = fDigitEndSkip + 0.7*(deconv.GetSampleCount()-2*fDigitEndSkip);
+    // A threshold will be set in terms of standard deviations of the noise.
+    // Peaks less than this are rejected as noise.
+    double noise = fWork[inoise];
+
+    // Protect against a "zero" channel.
+    if (noise < 10) {
+        CaptLog("Wire with no signal: " << deconv.GetChannelId()
+                << " noise: " << noise
+                << " max: " << fWork[deconv.GetSampleCount()-1]);
+        return wireCharge;
+    }
+    
+    // Don't bother with channels that have crazy big noise.  This says if the
+    // noise is bigger than the peak selection threshold, don't save the wire.
+    if (noise > peakAreaCut) {
+        CaptInfo("Wire with large peak noise: " << deconv.GetChannelId()
+                 << "  noise: " << noise);
+        // return wireCharge;
+    }
+
+#define STANDARD_HISTOGRAM
+#ifdef STANDARD_HISTOGRAM
+#undef STANDARD_HISTOGRAM
+    static TH1F* gNoiseHistogram = NULL;
+    if (!gNoiseHistogram) {
+        gNoiseHistogram
+            = new TH1F("peakSearchNoise",
+                       "Peak area sigma for all wires",
+                       100, 0.0, 5000);
+    }
+    gNoiseHistogram->Fill(noise);
+#endif
+
+    // Now use fWork to mask out peaks that are part of another peak.
+    std::fill(&fWork[0],&fWork[deconv.GetSampleCount()], 0.0);
+    
+    typedef std::pair<double, std::size_t> PeakPosition;
+
+    // Find all the possible peak positions and sort them so the biggest are
+    // last.
+    std::vector<PeakPosition> peakCandidates;
+    // Find the candidate peaks.
+    for (std::size_t i = fDigitEndSkip;
+         i<deconv.GetSampleCount()-fDigitEndSkip;
+         ++i) {
+        if (deconv.GetSample(i) < deconv.GetSample(i-1)) continue;
+        if (deconv.GetSample(i) < deconv.GetSample(i+1)) continue;
+#ifdef SKIP_A_BUNCH
+        if (i > 2*3450) continue;
+        if (i < 2*3400) continue;
+        if (CP::GeomId::Captain::IsVWire(id)) {
+            if (CP::GeomId::Captain::GetWireNumber(id) < 105) continue;
+            if (CP::GeomId::Captain::GetWireNumber(id) > 135) continue;
+        }
+        if (CP::GeomId::Captain::IsUWire(id)) {
+            if (CP::GeomId::Captain::GetWireNumber(id) < 170) continue;
+            if (CP::GeomId::Captain::GetWireNumber(id) > 190) continue;
+        }
+#endif
+        if (deconv.GetSample(i) < peakMaximumCut) continue;
+        peakCandidates.push_back(std::make_pair(deconv.GetSample(i), i));
+    }
+    std::sort(peakCandidates.begin(), peakCandidates.end());
+    
+    // The vector of peak positions that need to be made into hits.
+    std::vector< std::pair<int,int> > peaks;
+    
+    // Look at all of the peak candidates and find the "real" hits.
+    while (!peakCandidates.empty()) {
+        // The height of the current candidate
+        int candidateHeight = peakCandidates.back().first;
+        int candidateIndex = peakCandidates.back().second;
+        peakCandidates.pop_back();
+        // Skip peaks that close to other peaks.
+        if (fWork[candidateIndex] > 0.5) {
+            continue;
+        }
+        // Apply a cut based on the noise in the peak search.
+        // if (candidateHeight < noise*fNoiseThresholdCut) continue;
+        // Find the extent of the peak.
+        std::pair<int, int> extent = PeakExtent(candidateIndex,
+                                                deconv,
+                                                baselineSigma,
+                                                sampleSigma);
+        // Find the charge in the peak candidate.
+        double charge = 0.0;
+        for (int j=extent.first; j<=extent.second; ++j) {
+            double r = deconv.GetSample(j); 
+            charge += r;            
+        }
+        // Find the fwhm.
+        double fwhm = digitStep*PeakFWHM(candidateIndex, extent, deconv);
+#define STANDARD_HISTOGRAM
+#ifdef STANDARD_HISTOGRAM
+#undef STANDARD_HISTOGRAM
+        TH2F* peakAreaHeight = NULL;
+        CP::TGeometryId paId
+            = CP::TChannelInfo::Get().GetGeometry(deconv.GetChannelId());
+        if (CP::GeomId::Captain::IsUWire(paId)) peakAreaHeight=gPeakAreaHeightU;
+        if (CP::GeomId::Captain::IsVWire(paId)) peakAreaHeight=gPeakAreaHeightV;
+        if (CP::GeomId::Captain::IsXWire(paId)) peakAreaHeight=gPeakAreaHeightX;
+        peakAreaHeight->Fill(candidateHeight,charge);
+        TH2F* peakFWHMHeight = NULL;
+        if (CP::GeomId::Captain::IsUWire(paId)) peakFWHMHeight=gPeakFWHMHeightU;
+        if (CP::GeomId::Captain::IsVWire(paId)) peakFWHMHeight=gPeakFWHMHeightV;
+        if (CP::GeomId::Captain::IsXWire(paId)) peakFWHMHeight=gPeakFWHMHeightX;
+        peakFWHMHeight->Fill(candidateHeight,fwhm/unit::microsecond);
+        TH2F* peakFWHMArea = NULL;
+        if (CP::GeomId::Captain::IsUWire(paId)) peakFWHMArea=gPeakFWHMAreaU;
+        if (CP::GeomId::Captain::IsVWire(paId)) peakFWHMArea=gPeakFWHMAreaV;
+        if (CP::GeomId::Captain::IsXWire(paId)) peakFWHMArea=gPeakFWHMAreaX;
+        peakFWHMArea->Fill(charge,fwhm/unit::microsecond);
+#endif
+        // Apply a cut to the overall peak size. (The digit has had the
+        // baseline remove and is in units of charge).
+        if (candidateHeight<peakMaximumCut) {
+            continue;
+        }
+        // Apply a cut on the charge in the peak.
+        if (charge < peakAreaCut) {
+            continue;
+        }
+        // Apply a cut on the overall peak width.
+        if (fwhm < peakWidthCut) {
+            continue;
+        }
+        // This looks like a real hit, so save it for later.
+        peaks.push_back(extent);
+        // Stop if we are getting to many peaks.  The peaks are built in order
+        // of height, so this tends to keep the biggest hits.  This only cuts
+        // hits on really noisy wires.
+        if (fMaxPeaks > 0 && (std::size_t) fMaxPeaks <= peaks.size()) {
+            break;
+        }
+        // Mark these samples as used...
+        for (int j = extent.first; j <= extent.second; ++j) {
+            fWork[j] = 1.0;
+        }
+    }
+
+    // Make all the hits.  
+    CP::TMakeWireHit makeHit(fCorrectElectronLifetime,
+                             fCorrectCollectionEfficiency);
+    for(std::vector< std::pair<int,int> >::iterator p = peaks.begin();
+        p != peaks.end(); ++p) {
+        CP::THandle<CP::THit> newHit = makeHit(deconv, digitStep, t0,
+                                               baselineSigma,
+                                               sampleSigma,
+                                               p->first, p->second,
+                                               false);
+        wireCharge += newHit->GetCharge();
+        hits.push_back(newHit);
+    }
+
+    return wireCharge;
+}
---
+++ src/TWirePeaks.hxx
@@ -0,0 +1,126 @@
+#ifndef TWirePeaks_hxx_seen
+#define TWirePeaks_hxx_seen
+
+#include <THitSelection.hxx>
+#include <TCalibPulseDigit.hxx>
+
+namespace CP {
+    class TWirePeaks;
+};
+
+/// This takes a TCalibPulseDigit and turns it into one or more TDataHit
+/// object.  The hit will contain the time, and charge of the hit.  This has
+/// the option of applying the electron lifetime correction, or to just return
+/// the raw integrated charge.
+class CP::TWirePeaks {
+public:
+
+    /// If the parameter is true, then the electron lifetime is corrected.
+    /// This is the normal setting and equalizes the response across the
+    /// detector.  If the first parameter is false, then this runs in
+    /// "calibration" mode and the drift correction is not applied.  If the
+    /// second parameter is false, then this runs in calibration mode, and the
+    /// collection efficiency is not applied.
+    explicit TWirePeaks(bool correctDrift=true,
+                        bool correctEfficiency=true);
+    ~TWirePeaks();
+    
+    /// Take a TCalibPulseDigit object with the calibrated charges, and the
+    /// deconvolution of the calibrated charges, to find any hits in the
+    /// pulse.  Any hits that are found are appended to output THitSelection.
+    /// This takes a hit selection of PMT hits that will be used as a hint for
+    /// the expected dispersion of the wire signals.  If the sample
+    /// uncertainty is provided, this is the uncertainty of each measurement
+    /// in the TCalibPulseDigit.  This returns the total charge on the wire.
+    double operator () (CP::THitSelection& hits, 
+                      const CP::TCalibPulseDigit& calib,
+                      const CP::TCalibPulseDigit& deconv,
+                      double t0,
+                      double baseSigma = 0.0,
+                      double sampleSigma = 0.0);
+
+private:
+
+    /// Determine the bounds of the peak.  This returns a pair with the first
+    /// and last indices in the peak (inclusive) i.e. [begin .. end], not the
+    /// usual [begin .. end).
+    std::pair<int, int> PeakExtent(int peakIndex,
+                                   const CP::TCalibPulseDigit& deconv,
+                                   double baselineSigma, double sampleSigma);
+
+
+    /// Determine the FWHM of a peak given the peak bin.
+    double PeakFWHM(int peakIndex,
+                    const std::pair<int,int>& extent,
+                    const CP::TCalibPulseDigit& deconv);
+    
+    /// If this is true, then the electron lifetime correction is applied.
+    /// The correction should normally be applied, but for certain
+    /// calibrations it needs to be turned off.  This is controlled in the
+    /// constructor.
+    bool fCorrectElectronLifetime;
+
+    /// If this is true, then the collection efficiency correction is applied.
+    /// The correction should normally be applied, but for certain
+    /// calibrations it needs to be turned off.  This is controlled in the
+    /// constructor.
+    bool fCorrectCollectionEfficiency;
+
+    /// A buffer for local work.
+    std::vector<float> fWork;
+
+    /// The required charge in the sample at the peak required for it to be
+    /// considered valid.  This is pedestal subtracted and after the response
+    /// function has been deconvoluted.
+    double fPeakMaximumCol;
+    double fPeakMaximumInd;
+
+    /// The required total charge in the peak.
+    double fPeakAreaCol;
+    double fPeakAreaInd;
+
+    /// The expected minimum width for a drifting point charge.  The
+    /// deconvolution removes the electronics response, but does not remove
+    /// the width introduced by the drift physics.
+    double fPeakWidthCol;
+    double fPeakWidthInd;
+    
+    /// The noise threshold in "RMS" of the measured noise for the channel
+    /// (i.e. "sigma" fluctuation before it's not considered noise).  The
+    /// noise is not Gaussian, so a 3 sigma cut isn't 99.8 percent.
+    double fNoiseThresholdCut;
+    
+    /// Set the limit on how wide a peak can be in drift time before it's
+    /// split into multiple hits.  Peaks wider than this are split up into
+    /// multiple hits by drift time.
+    double fPeakRMSLimit;
+
+    /// The maximum number of hits allowed per wire.
+    int fMaxPeaks;
+
+    /// The number of samples to skip at the beginning and ending of the
+    /// digit.  This is needed since the first and last run of samples are
+    /// contaminated by FFT "wrap around".  There should not be any signal in
+    /// that part of the event anyway.
+    int fDigitEndSkip;
+    
+    /// Once a peak has been found, the charge is calculated by summing "out
+    /// from the peak" until the sample charges are below the charge
+    /// threshold.  This sets a threshold as a fraction of the peak height.
+    double fIntegrationChargeThreshold;
+
+    /// Once a peak has been found, the charge is calculated by summing "out
+    /// from the peak" until the sample charges are below the charge
+    /// thresholds.  This sets a threshold as a fraction of the peak height
+    /// below which a minimum will be taken as the boundary of the peak.
+    double fIntegrationShoulderThreshold;
+
+    /// Once a peak has been found, the charge is calculated by summing "out
+    /// from the peak" until the sample charges are below the charge
+    /// thresholds.  This sets a threshold as a fraction of the peak height
+    /// above which a new peak is assumed to be starting.
+    double fIntegrationValleyThreshold;
+
+};
+
+#endif
