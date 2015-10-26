bcfed6732ed10a41a17e49f1fdab3623a73ef6ff
Clark McGrew <clark.mcgrew@stonybrook.edu>
2015-10-26 10:19:19 -0400

    Improve the calculation of the median.
    The new calculate interpolates between integer ADC values.

--- src/TPulseCalib.cxx
+++ src/TPulseCalib.cxx
@@ -36,7 +36,7 @@ CP::TPulseCalib::operator()(const CP::TDigitProxy& digit) {
         + timeOffset;
     CP::TCalibPulseDigit::Vector samples(pulse->GetSampleCount());
 
-    // Use the median as the pedestal
+    // Use the median ADC value as the pedestal
     for (std::size_t i=0; i< pulse->GetSampleCount(); ++i) {
         double p = pulse->GetSample(i);
         if (!std::isfinite(p)) continue;
@@ -44,7 +44,13 @@ CP::TPulseCalib::operator()(const CP::TDigitProxy& digit) {
     }
     std::sort(samples.begin(), samples.end());
     int iMedian = pulse->GetSampleCount()/2;
-    fPedestal = samples[iMedian];
+    std::pair<CP::TCalibPulseDigit::Vector::iterator,
+              CP::TCalibPulseDigit::Vector::iterator> bounds;
+    bounds = std::equal_range(samples.begin(), samples.end(),
+                              samples[iMedian]);
+    fPedestal = (samples.begin()+iMedian)-bounds.first;
+    fPedestal /= bounds.second-bounds.first;
+    fPedestal += std::min(samples[iMedian]-0.5,0.0);
 
     // Calculate the RMS around the average with the tails thrown out.
     fAverage = 0.0;
@@ -65,14 +71,25 @@ CP::TPulseCalib::operator()(const CP::TDigitProxy& digit) {
     if (fSigma>0) fSigma = std::sqrt(fSigma);
     else fSigma = - std::sqrt(-fSigma);
 
-    // Calculate the channel-to-channel Gaussian sigma.
+    // Calculate the channel-to-channel Gaussian sigma.  The 52 percentile is
+    // a of the sample-to-sample differences is a good estimate of the
+    // distribution sigma.  The 52% "magic" number comes looking at the
+    // differences of two samples (that gives a sqrt(2)) and wanting the RMS
+    // (which is the 68%).  This gives 48% (which is 68%/sqrt(2)).  Then
+    // because of the ordering after the sort, the bin we look at is 1.0-52%.
     for (std::size_t i=1; i< pulse->GetSampleCount(); ++i) {
         samples[i] = std::abs(pulse->GetSample(i)-pulse->GetSample(i-1));
     }
     samples[0] = 0.0;
     std::sort(samples.begin(), samples.end());
     iMedian = 0.52*pulse->GetSampleCount();
-    fGaussianSigma = samples[iMedian];
+    std::pair<CP::TCalibPulseDigit::Vector::iterator,
+              CP::TCalibPulseDigit::Vector::iterator> gaussianBounds;
+    gaussianBounds = std::equal_range(samples.begin(), samples.end(),
+                              samples[iMedian]);
+    fGaussianSigma = (samples.begin()+iMedian)-gaussianBounds.first;
+    fGaussianSigma /= gaussianBounds.second-gaussianBounds.first;
+    fGaussianSigma += std::min(samples[iMedian]-0.5,0.0);
 
     // Actually apply the calibration.
     for (std::size_t i=0; i< pulse->GetSampleCount(); ++i) {
